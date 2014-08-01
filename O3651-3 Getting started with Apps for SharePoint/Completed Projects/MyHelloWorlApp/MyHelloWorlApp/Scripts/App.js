cafc78a07429449f3969a50202e730f3b959938a
Scot Hillier <scot@scothillier.net>
2014-08-01 19:07:40 -0400

    First 5 Modules

---
+++ O3651-3 Getting started with Apps for SharePoint/Completed Projects/MyHelloWorlApp/MyHelloWorlApp/Scripts/App.js
@@ -0,0 +1,14 @@
+﻿	'use strict';
+
+	$(document).ready(onPageLoad);
+
+	function onPageLoad() {
+		$("#message").text("Hello from the document ready event handler");
+		$("#cmdPushMe").click(onButtonClick);
+	}
+
+	function onButtonClick() {
+		$("#displayDiv")
+			.text("Hello Apps")
+			.css({ "margin": "16px", "color": "green", "font-size": "32px" });
+	}
