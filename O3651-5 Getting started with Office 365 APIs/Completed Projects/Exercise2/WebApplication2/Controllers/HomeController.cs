1f4e2c8a45f4dd7473e623f5ddbd5218b803dca5
Scot Hillier <scot@scothillier.net>
2014-08-07 17:54:21 -0400

    Update for latest O365APIs

---
+++ O3651-5 Getting started with Office 365 APIs/Completed Projects/Exercise2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web;
+using System.Web.Mvc;
+
+using System.Threading.Tasks;
+using Microsoft.Office365.Exchange;
+using Microsoft.Office365.OAuth;
+
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                IOrderedEnumerable<IEvent> events = await CalendarAPISample.GetCalendarEvents();
+                ViewBag.Events = events;
+            }
+            catch (RedirectRequiredException x)
+            {
+                return Redirect(x.RedirectUri.ToString());
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
