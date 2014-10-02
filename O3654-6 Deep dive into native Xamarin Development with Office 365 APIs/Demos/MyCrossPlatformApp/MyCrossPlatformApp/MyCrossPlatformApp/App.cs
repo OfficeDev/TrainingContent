a6c4e3369b437c6e185c294be2bded3d40fdd1ae
Ted Pattison <ted@tedpattison.net>
2014-10-02 11:37:18 -0400

    Xamarin Updates

---
+++ O3654-6 Deep dive into native Xamarin Development with Office 365 APIs/Demos/MyCrossPlatformApp/MyCrossPlatformApp/MyCrossPlatformApp/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MyCrossPlatformApp {
+  public class App {
+    public static Page GetMainPage() {
+      return new ContentPage {
+        Content = new Label {
+          Text = "Hello, Forms !",
+          VerticalOptions = LayoutOptions.CenterAndExpand,
+          HorizontalOptions = LayoutOptions.CenterAndExpand,
+        },
+      };
+    }
+  }
+}
