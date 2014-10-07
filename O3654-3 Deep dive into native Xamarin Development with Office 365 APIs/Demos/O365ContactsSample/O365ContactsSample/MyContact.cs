b3a037bd6cf40596abfb80f6c384e797afaa9a35
Ted Pattison <ted@tedpattison.net>
2014-10-07 16:21:36 -0700

    Updated demos presented y Ted while recording the Xamarin module

---
+++ O3654-3 Deep dive into native Xamarin Development with Office 365 APIs/Demos/O365ContactsSample/O365ContactsSample/MyContact.cs
@@ -0,0 +1,9 @@
+namespace O365ContactsSample
+{
+    public class MyContact
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
