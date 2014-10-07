3060301447bcbbbbe17d390f22a6c813016c6959
Ted Pattison <ted@tedpattison.net>
2014-10-07 16:18:28 -0700

    Updated demos presented by Ted while recording Cordova module

---
+++ O3654-2 Deep dive into Mobile Development with Office 365 and Cordova/Demos/Cheater/index.js.txt
@@ -0,0 +1,52 @@
+﻿
+
+(function () {
+  "use strict";
+
+  document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+  function onDeviceReady() {
+    // Handle the Cordova pause and resume events
+    document.addEventListener('pause', onPause.bind(this), false);
+    document.addEventListener('resume', onResume.bind(this), false);
+
+    // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    $(function () {
+       navigator.geolocation.getCurrentPosition(onPositionSuccess);
+       $("#showMap").click(showMap);
+    })
+  };
+
+  function onPause() { };
+  function onResume() { };
+
+  var currentPosition;
+
+  function onPositionSuccess(position) {
+    currentPosition = position
+    $('#lat').text(position.coords.latitude);
+    $('#long').text(position.coords.longitude);
+  }
+
+
+  function showMap() {
+    var googlePosition = new google.maps.LatLng(currentPosition.coords.latitude,
+                                                currentPosition.coords.longitude);
+
+    var mapOptions = {
+      sensor: true,
+      center: googlePosition,
+      panControl: false,
+      zoomControl: true,
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: false,
+      mapTypeControl: true
+    }
+
+    var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
+    var marker = new google.maps.Marker({ position: googlePosition, map: map });
+
+  }
+
+})();
---
+++ O3654-2 Deep dive into Mobile Development with Office 365 and Cordova/Demos/MyO365CordovaApp/MyO365CordovaApp/services/office365/scripts/typings/o365discovery.d.ts
@@ -0,0 +1,84 @@
+﻿declare module O365Discovery {
+    var deferred: <T>() => Microsoft.Utility.IDeferred<T>;
+    class Request {
+        public requestUri: string;
+        public headers: {
+            [name: string]: string;
+        };
+        public method: string;
+        public data: any;
+        public disableCache: boolean;
+        constructor(requestUri: string);
+    }
+    var capabilityScopes: {
+        AllSites: {
+            Read: string;
+            Write: string;
+            Manage: string;
+            FullControl: string;
+        };
+        MyFiles: {
+            Read: string;
+            Write: string;
+        };
+        user_impersonation: string;
+        full_access: string;
+        Mail: {
+            Read: string;
+            Write: string;
+            Sent: string;
+        };
+        Calendars: {
+            Read: string;
+            Write: string;
+        };
+        Contacts: {
+            Read: string;
+            Write: string;
+        };
+    };
+    enum AccountType {
+        MicrosoftAccount = 1,
+        OrganizationalId = 2,
+    }
+    interface IFirstSignIn {
+        user_email: string;
+        account_type: AccountType;
+        authorization_service: string;
+        token_service: string;
+        scope: string;
+        unsupported_scope: string;
+        discovery_service: string;
+        discovery_resource: string;
+    }
+    interface ICapabilityResult {
+        Capability: string;
+        EntityKey: string;
+        ProviderId: string;
+        ProviderName: string;
+        ServiceAccountType: AccountType;
+        ServiceEndpointUri: string;
+        ServiceId: string;
+        ServiceName: string;
+        ServiceResourceId: string;
+    }
+    class ServiceCapability {
+        private _result;
+        constructor(result: ICapabilityResult);
+        public capability : string;
+        public endpointUri : string;
+        public name : string;
+        public resourceId : string;
+    }
+    class Context {
+        private _discoveryUri;
+        private _redirectUri;
+        constructor(redirectUri?: string);
+        private getDeferred<T>();
+        private ajax(request);
+        private getParameterByName(url, name);
+        public firstSignIn(scopes: string, redirectUri?: string): Microsoft.Utility.IPromise<IFirstSignIn>;
+        public services(getAccessTokenFn: () => Microsoft.Utility.IPromise<string>): Microsoft.Utility.IPromise<ServiceCapability[]>;
+        public allServices(): void;
+    }
+}
