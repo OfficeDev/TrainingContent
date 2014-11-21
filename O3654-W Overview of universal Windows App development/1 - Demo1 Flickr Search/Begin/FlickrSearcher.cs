a0ede99df2b402f0bbbf166bbd4424644e1b507a
Jeremy Thake <jeremythake@jthake-macselfhostcorpmicrosoftcom.local>
2014-11-21 12:35:20 -0500

    Adding O3654-W back to master and also promoting O3657-1 to master too.

---
+++ O3654-W Overview of universal Windows App development/1 - Demo1 Flickr Search/Begin/FlickrSearcher.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace FlickrSearch
+{
+
+  public class FlickrPhotoResult
+  {
+    public FlickrPhotoResult(XElement photo)
+    {
+      Id = (long)photo.Attribute("id");
+      Secret = photo.Attribute("secret").Value;
+      Farm = (int)photo.Attribute("farm");
+      Server = (int)photo.Attribute("server");
+      Title = photo.Attribute("title").Value;
+    }
+    public long Id { get; private set; }
+    public string Secret { get; private set; }
+    public int Farm { get; private set; }
+    public string Title { get; private set; }
+    public int Server { get; private set; }
+
+    public string ImageUrl
+    {
+      get
+      {
+        return (string.Format(
+          "https://farm{0}.static.flickr.com/{1}/{2}_{3}_m.jpg",
+          Farm, Server, Id, Secret));
+      }
+    }
+  }
+  public static class FlickrSearcher
+  {
+    public static async Task<List<FlickrPhotoResult>> SearchAsync(string searchTerm)
+    {
+      HttpClient client = new HttpClient();
+      FlickrSearchUrl url = new FlickrSearchUrl(searchTerm);
+      List<FlickrPhotoResult> list = new List<FlickrPhotoResult>();
+
+      using (HttpResponseMessage response = await client.GetAsync(new Uri(url.ToString(), UriKind.Absolute)))
+      {
+        if (response.IsSuccessStatusCode)
+        {
+            String contentxml = await response.Content.ReadAsStringAsync();
+            XElement xml = XElement.Parse(contentxml);
+            list =
+                (
+                    from p in xml.DescendantsAndSelf("photo")
+                    select new FlickrPhotoResult(p)
+                ).ToList();
+        }
+      }
+      return (list);
+    }
+    public static async Task<List<string>> GetHotlistTagsAsync(string userText)
+    {
+      HttpClient client = new HttpClient();
+      string uri = FlickrSearchUrl.hottagsUri;
+      List<string> results = new List<string>();
+
+      using (HttpResponseMessage response = await client.GetAsync(new Uri(uri.ToString(), UriKind.Absolute)))
+      {
+          if (response.IsSuccessStatusCode)
+          {
+              String contentxml = await response.Content.ReadAsStringAsync();
+              XElement xml = XElement.Parse(contentxml);
+              results =
+                (
+                  from tag in xml.DescendantsAndSelf("tag")
+                  where tag.Value.Contains(userText)
+                  select tag.Value
+                ).ToList();
+          }
+      }
+      return (results.GetRange(0, Math.Min(5, results.Count)));
+    }
+
+    #region Internal_Class
+    internal class FlickrSearchUrl
+    {
+//#error Add your flickR API key here
+      static string apiKey = "042bddb721dbc8cd4dadb237bfd4624f";
+      static string serviceUri = "https://api.flickr.com/services/rest/?method=";
+      static string baseUri = serviceUri + "flickr.photos.search&";
+      public static readonly string hottagsUri = serviceUri + "flickr.tags.getHotList&api_key=" + apiKey;
+
+      public int ContentType { get; set; }
+      public int PerPage { get; set; }
+      public int Page { get; set; }
+      public string SearchTerm { get; set; }
+
+      public FlickrSearchUrl(
+          string searchTerm,
+          int pageNo = 1,
+          int perPage = 50,
+          int contentType = 1
+      )
+      {
+        this.SearchTerm = searchTerm;
+        this.Page = pageNo;
+        this.PerPage = perPage;
+        this.ContentType = contentType;
+      }
+      public override string ToString()
+      {
+        return (
+          string.Format(
+            baseUri +
+            "api_key={0}&" +
+            "safe_search=1&" +
+            "text={1}&" +
+            "page={2}&" +
+            "per_page={3}&" +
+            "content_type={4}",
+            apiKey, this.SearchTerm, this.Page, this.PerPage, this.ContentType));
+      }
+    }
+    #endregion
+
+  }
+}
---
+++ O3654-W Overview of universal Windows App development/2 - Session 3 Demos/5 - Design-time Data END/ContosoCookbookSimple/ContosoCookbookSimple/DataModel/SampleDataSource.cs
@@ -0,0 +1,146 @@
+﻿// ----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+// 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+// ----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The data model defined by this file serves as a representative example of a strongly-typed
+// model that supports notification when members are added, removed, or modified.  The property
+// names chosen coincide with data bindings in the standard item templates.
+//
+// Applications may use this model as a starting point and build on it, or discard it entirely and
+// replace it with something appropriate to their needs.
+
+namespace ContosoCookbookSimple.DataModel
+{
+    /// <summary>
+    /// Base class for <see cref="SampleDataItem"/> and <see cref="SampleDataGroup"/> that
+    /// defines properties common to both.
+    /// </summary>
+    [Windows.Foundation.Metadata.WebHostHidden]
+    public abstract class SampleDataCommon : BindableBase
+    {
+        private static Uri _baseUri = new Uri("ms-appx:///");
+
+        public SampleDataCommon(String uniqueId, String title, String subtitle, String imagePath, String description)
+        {
+            this._uniqueId = uniqueId;
+            this._title = title;
+            this._shorttitle = subtitle;
+            this._description = description;
+            this._imagePath = imagePath;
+        }
+
+        private string _uniqueId = string.Empty;
+        public string UniqueId
+        {
+            get { return this._uniqueId; }
+            set { this.SetProperty(ref this._uniqueId, value); }
+        }
+
+        private string _title = string.Empty;
+        public string Title
+        {
+            get { return this._title; }
+            set { this.SetProperty(ref this._title, value); }
+        }
+
+        private string _shorttitle = string.Empty;
+        public string ShortTitle
+        {
+            get { return this._shorttitle; }
+            set { this.SetProperty(ref this._shorttitle, value); }
+        }
+
+        private string _description = string.Empty;
+        public string Description
+        {
+            get { return this._description; }
+            set { this.SetProperty(ref this._description, value); }
+        }
+
+        private ImageSource _image = null;
+        private String _imagePath = null;
+
+        public Uri ImagePath
+        {
+            get
+            {
+                return new Uri(RecipeDataCommon._baseUri, this._imagePath);
+            }
+        }
+        public ImageSource Image
+        {
+            get
+            {
+                if (this._image == null && this._imagePath != null)
+                {
+                    this._image = new BitmapImage(new Uri(SampleDataCommon._baseUri, this._imagePath));
+                }
+                return this._image;
+            }
+
+            set
+            {
+                this._imagePath = null;
+                this.SetProperty(ref this._image, value);
+            }
+        }
+
+        public void SetImage(String path)
+        {
+            this._image = null;
+            this._imagePath = path;
+            this.OnPropertyChanged("Image");
+        }
+    }
+
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class SampleDataItem : SampleDataCommon
+    {
+        public SampleDataItem() : base(
+                    "Sample Data Item",
+                    "Item Title: 1",
+                    "Item Subtitle: 1",
+                    "Assets/LightGray.png",
+                    "Item Description: Pellentesque porta, mauris quis interdum vehicula, urna sapien ultrices velit, nec venenatis dui odio in augue. Cras posuere, enim a cursus convallis, neque turpis malesuada erat, ut adipiscing neque tortor ac erat."
+                    )
+        {
+            this._directions = "Curabitur class aliquam vestibulum nam curae maecenas sed integer cras phasellus suspendisse quisque donec dis praesent";
+        }
+
+        private string _directions = string.Empty;
+        public string Directions
+        {
+            get { return this._directions; }
+            set { this.SetProperty(ref this._directions, value); }
+        }
+
+        private int _preptime = 240;
+        public int PrepTime
+        {
+            get { return this._preptime; }
+            set { this.SetProperty(ref this._preptime, value); }
+        }
+
+    }
+}
