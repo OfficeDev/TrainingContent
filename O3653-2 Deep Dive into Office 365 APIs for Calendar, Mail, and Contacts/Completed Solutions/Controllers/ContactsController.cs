6806490a97d81dca25e6f514471b3dc391b485fd
Andrew Connell <me@andrewconnell.com>
2014-11-25 17:00:28 -0500

    added final lab solution

---
+++ O3653-2 Deep Dive into Office 365 APIs for Calendar, Mail, and Contacts/Completed Solutions/Controllers/ContactsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Office365Contacts.Models;
+
+namespace Office365Contacts.Controllers {
+  public class ContactsController : Controller {
+    [Authorize]
+    public async Task<ActionResult> Index(int? pageNumber) {
+      // setup paging control
+      int pageSize = 8;
+      int pageIndex = (pageNumber != null) ? (int)pageNumber - 1 : 0;
+      ViewBag.pageIndex = pageIndex;
+      ViewBag.pageSize = pageSize;
+
+      // get a list of all contacts
+      List<MyContact> contacts = null;
+      MyContactRepository contactRepository = new MyContactRepository();
+      ViewBag.ContactCount = await contactRepository.GetContactCount();
+      contacts = await contactRepository.GetContacts(pageIndex, pageSize);
+
+      // pass the collection of contacts to the view in the model
+      return View(contacts);
+    }
+
+    [Authorize]
+    public async Task<ActionResult> Delete(string id) {
+      MyContactRepository contactRepository = new MyContactRepository();
+      if (id != null) {
+        await contactRepository.DeleteContact(id);
+      }
+      return Redirect("/");
+    }
+
+    [Authorize]
+    public async Task<ActionResult> Create(MyContact myContact) {
+      // if a contact was submitted, create it
+      if (Request.HttpMethod == "POST") {
+        MyContactRepository contactRepository = new MyContactRepository();
+        await contactRepository.AddContact(myContact);
+        return Redirect("/");
+        // else create a empty model & return to the create page view
+      } else {
+        return View(myContact);
+      }
+    }
+
+  }
+}
