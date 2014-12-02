0d31f81974dcf0df7a70b34b7d6ddecdd9cd9ea1
Andrew Connell <me@andrewconnell.com>
2014-12-01 21:16:45 -0500

    added completed lab

---
+++ O3653-4 Deep Dive into Office 365 APIs for SharePoint Site services/Demos/SPContactsList/Controllers/SpContactController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SPContactsList.Models;
+
+namespace SPContactsList.Controllers
+{
+    public class SpContactController : Controller
+    {
+      [Authorize]
+      public async Task<ActionResult> Index(int? pageIndex, int? pageSize, string contactId) {
+        SpContactRepository repository = new SpContactRepository();
+
+        if (Request.HttpMethod == "POST" && contactId != null) {
+          await repository.Delete(contactId);
+          return Redirect("/");
+        }
+
+        SpContactViewModel model = new SpContactViewModel();
+
+        if (pageIndex == null) {
+          model.PageIndex = 0;
+        } else {
+          model.PageIndex = (int)pageIndex;
+        }
+
+        if (pageSize == null) {
+          model.PageSize = 10;
+        } else {
+          model.PageSize = (int)pageSize;
+        }
+
+        model.SpContacts = await repository.GetContacts(model.PageIndex, model.PageSize);
+
+        return View(model);
+      }
+
+      [Authorize] 
+      public async Task<ActionResult> Details(string contactId) {
+        SpContactRepository repository = new SpContactRepository();
+
+        SpContact task = await repository.GetTask(contactId);
+
+        return View(task);
+      }
+
+      [Authorize]
+      public async Task<ActionResult> Create(SpContact task) {
+        SpContactRepository repository = new SpContactRepository();
+
+        if (Request.HttpMethod == "POST") {
+          await repository.CreateTask(task);
+          return Redirect("/");
+        } else {
+          return View(task);
+        }
+      }
+
+      [Authorize]
+      public async Task<ActionResult> Edit(string Id, SpContact task) {
+        SpContactRepository repository = new SpContactRepository();
+
+        if (Request.HttpMethod == "POST") {
+          await repository.UpdateTask(task);
+          return Redirect("/");
+        } else {
+          task = await repository.GetTask(Id);
+          return View(task);
+        }
+      }
+    }
+  }
---
+++ O3653-4 Deep Dive into Office 365 APIs for SharePoint Site services/Demos/SPContactsList/Data/SpContactsListContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using SPContactsList.Models;
+
+namespace SPContactsList.Data {
+
+  public class SpContactsListContext : DbContext {
+    public SpContactsListContext()
+      : base("SPContactsListContext") { }
+
+    //public DbSet<Tenant> Tenants { get; set; }
+    //public DbSet<User> Users { get; set; }
+
+    public DbSet<PerWebUserCache> PerUserCacheList { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+      modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    }
+  }
+}
