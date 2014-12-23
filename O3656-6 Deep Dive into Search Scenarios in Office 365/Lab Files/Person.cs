060b243875ac72c114c868e0840b6f245484f803
ScotHillier <scot@scothillier.net>
2014-12-23 08:59:46 -0500

    O3656-6 Deep Dive into Search Scenarios

---
+++ O3656-6 Deep Dive into Search Scenarios in Office 365/Lab Files/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDirectoryWeb.Models
+{
+    public class Person
+    {
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("E-mail Address")]
+        public string WorkEmail { get; set; }
+
+        [DisplayName("Phone")]
+        public string WorkPhone { get; set; }
+
+    }
+}
