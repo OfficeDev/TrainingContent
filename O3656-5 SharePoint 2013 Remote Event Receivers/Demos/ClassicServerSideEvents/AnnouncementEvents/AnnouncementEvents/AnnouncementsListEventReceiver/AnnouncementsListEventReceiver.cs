f6bb00cee91da4bdba7f6b5d9448581663fcef5e
Ted Pattison <ted@tedpattison.net>
2015-01-08 10:53:32 -0800

    O3656-5 Post Recording

---
+++ O3656-5 SharePoint 2013 Remote Event Receivers/Demos/ClassicServerSideEvents/AnnouncementEvents/AnnouncementEvents/AnnouncementsListEventReceiver/AnnouncementsListEventReceiver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace AnnouncementEvents.AnnouncementsListEventReceiver {
+
+  public class AnnouncementsListEventReceiver : SPItemEventReceiver {
+
+    public override void ItemAdding(SPItemEventProperties properties) {
+
+      // examine user input
+      string title = properties.AfterProperties["Title"].ToString();
+
+      // validate user input
+      if (title.ToLower().Contains("lobster") || title.ToLower().Contains("clam")) {
+        // cancel user action if inout is invalid
+        properties.Status = SPEventReceiverStatus.CancelWithError;
+        properties.ErrorMessage = "Do not use inflamitory terms such as 'lobster' or'clam'.";
+      }
+    }
+
+    public override void ItemUpdating(SPItemEventProperties properties) {
+      string title = properties.AfterProperties["Title"].ToString();
+      if (title.ToLower().Contains("lobster") || title.ToLower().Contains("clam")) {
+        properties.Status = SPEventReceiverStatus.CancelWithError;
+        properties.ErrorMessage = "Do not use inflamitory terms such as 'lobster' or'clam'.";
+      }
+    }
+
+    public override void ItemAdded(SPItemEventProperties properties) {
+
+      // retrieve user inolut from content database
+      string title = properties.ListItem["Title"].ToString();
+      
+      // convert the title to upper case if required
+      if (!title.ToUpper().Equals(title)) {
+        this.EventFiringEnabled = true;
+        properties.ListItem["Title"] = title.ToUpper();
+        properties.ListItem.UpdateOverwriteVersion();
+        this.EventFiringEnabled = false;
+      }
+    }
+
+
+    public override void ItemUpdated(SPItemEventProperties properties) {
+      string title = properties.ListItem["Title"].ToString();
+      if (!title.ToUpper().Equals(title)) {
+        this.EventFiringEnabled = true;
+        properties.ListItem["Title"] = title.ToUpper();
+        properties.ListItem.UpdateOverwriteVersion();
+        this.EventFiringEnabled = false;
+      }
+    }
+
+  }
+}
---
+++ O3656-5 SharePoint 2013 Remote Event Receivers/Demos/DebuggingRemoteEvents/RemoteEventsLabWeb/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RemoteEventsLabWeb.Controllers {
+  public class HomeController : Controller {
+
+    [SharePointContextFilter]
+    public ActionResult Index() {
+
+      var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+
+      using (var clientContext = spContext.CreateUserClientContextForSPHost()) {
+        if (clientContext != null) {
+          clientContext.Load(clientContext.Web);
+          ListCollection Lists = clientContext.Web.Lists;
+          clientContext.Load(Lists, lists => lists.Where(list => (!list.Hidden) && (list.BaseType == 0))
+                                                  .Include(list => list.Title,
+                                                           list => list.DefaultViewUrl));
+          clientContext.ExecuteQuery();
+
+          Dictionary<string, string> userLists = new Dictionary<string, string>();
+
+          string siteUrl = clientContext.Web.Url;
+          foreach (var list in Lists) {
+            userLists.Add(siteUrl + list.DefaultViewUrl, list.Title);
+          }
+
+          ViewBag.UserLists = userLists;
+        }
+      }
+      return View();
+    }
+    
+    public ActionResult About() {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact() {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
+}
