b54082ecf246d7931263e57087164096d5084ed7
Andrew Connell <me@andrewconnell.com>
2015-05-27 16:51:34 -0400

    o3653-8: added lab files for template project

---
+++ O3653/O3653-8 Deep Dive into the Office 365 Unified API/Lab Files/Data/TokenCacheDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using UnifiedApiApp.Models;
+
+namespace UnifiedApiApp.Data {
+  public class TokenCacheDataContext : DbContext{
+    public TokenCacheDataContext()
+      : base("TokenCacheDataContext") { }
+
+    public DbSet<PerUserWebCache> PerUserCacheList { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+      modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    }
+  }
+}
---
+++ O3653/O3653-8 Deep Dive into the Office 365 Unified API/Lab Files/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UnifiedApiApp.MvcApplication" Language="C#" %>
