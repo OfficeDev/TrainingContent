e4c59d200ae0b4561aa0b66e53378651b03ff262
Andrew Connell <me@andrewconnell.com>
2015-05-27 17:14:46 -0400

    o3653-8: added starter project for lab solution

---
+++ O3653/O3653-8 Deep Dive into the Office 365 Unified API/Completed project/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using UnifiedApiApp.Data;
+
+namespace UnifiedApiApp {
+  public class MvcApplication : System.Web.HttpApplication {
+    protected void Application_Start() {
+      AreaRegistration.RegisterAllAreas();
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+      // set the db initializer
+      Database.SetInitializer(new TokenCacheInitializer());
+    }
+  }
+}
---
+++ O3653/O3653-8 Deep Dive into the Office 365 Unified API/Completed project/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(UnifiedApiApp.Startup))]
+
+namespace UnifiedApiApp {
+  public partial class Startup {
+    public void Configuration(IAppBuilder app)
+    {
+      ConfigureAuth(app);
+    }
+  }
+}
