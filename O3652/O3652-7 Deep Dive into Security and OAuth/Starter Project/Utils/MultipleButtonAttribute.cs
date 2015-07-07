abf2d4952ecf27708a9251e40a08286e69a2b4ef
Andrew Connell <me@andrewconnell.com>
2015-07-07 14:12:48 -0400

    o3652-7: added started project for HOL EX3

---
+++ O3652/O3652-7 Deep Dive into Security and OAuth/Starter Project/Utils/MultipleButtonAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientCredsAddin.Utils {
+  [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+  public class MultipleButtonAttribute : ActionNameSelectorAttribute {
+    public string Name { get; set; }
+    public string Argument { get; set; }
+
+    public override bool IsValidName(ControllerContext controllerContext, string actionName, MethodInfo methodInfo) {
+      var isValidName = false;
+      var keyValue = string.Format("{0}:{1}", Name, Argument);
+      var value = controllerContext.Controller.ValueProvider.GetValue(keyValue);
+
+      if (value != null) {
+        controllerContext.Controller.ControllerContext.RouteData.Values[Name] = Argument;
+        isValidName = true;
+      }
+
+      return isValidName;
+    }
+  }
+}
