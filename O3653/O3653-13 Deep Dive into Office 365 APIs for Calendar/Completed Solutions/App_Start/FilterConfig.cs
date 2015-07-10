a6d939db6c5bff86b806e6a1896e0b5ced162d9f
Andrew Connell <me@andrewconnell.com>
2015-07-10 06:37:54 -0400

    o3653-13: added HOL for calendar module

---
+++ O3653/O3653-13 Deep Dive into Office 365 APIs for Calendar/Completed Solutions/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Office365Calendar {
+  public class FilterConfig {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
