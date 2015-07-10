d9f13905e689dc44681342871e497ca7401dbaab
Andrew Connell <me@andrewconnell.com>
2015-07-10 15:43:21 -0400

    o3653-14: added final solution for lab and lab help files

---
+++ O3653/O3653-14 Deep Dive into Office 365 APIs for Contacts/Completed Solutions/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Office365Contact.Startup))]
+
+namespace Office365Contact {
+  public partial class Startup {
+
+    public void Configuration(IAppBuilder app)
+    {
+      ConfigureAuth(app);
+    }
+
+  }
+}
---
+++ O3653/O3653-14 Deep Dive into Office 365 APIs for Contacts/Lab Files/Office365ContactInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Office365Contact.Data {
+  public class Office365ContactInitializer : System.Data.Entity.DropCreateDatabaseAlways<Office365ContactContext> {
+
+  }
+
+}
