6c79ae7c0df78935e4282ac0c97409e98af11681
Todd Baginski <topshelftbag@hotmail.com>
2015-12-21 15:12:37 -0500

    Content updates for O3651-2. Updated app to Add-ins. Updated Projects to Visual Studio 2015.

---
+++ O3651/O3651-2 Getting started with Apps for Office/Completed Projects/ContentGenerator - Exercise 1/ContentGeneratorWeb/AddIn/Home/Home.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#get-data-from-selection').click(getDataFromSelection);
+        });
+    };
+
+    // Reads data from current document selection and displays a notification
+    function getDataFromSelection() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    app.showNotification('The selected text is:', '"' + result.value + '"');
+                } else {
+                    app.showNotification('Error:', result.error.message);
+                }
+            }
+        );
+    }
+})();
