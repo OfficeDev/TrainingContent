d34148742be59a178fcca4b2a8d04ee0ac531a8e
Todd Baginski <topshelftbag@hotmail.com>
2015-12-22 15:57:32 -0500

    Added completed project for exercise 3.

---
+++ O3651/O3651-2 Getting started with Apps for Office/Completed Projects/ContentGenerator - Exercise 3/ContentGeneratorWeb/AddIn/Home/Home.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    "use strict";
+
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+            // adding event handlers for app command buttons
+            $("#cmdGetContent").click(cmdGetContent);
+            $("#cmdInsertContent").click(cmdInsertContent);
+        });
+    };
+
+    function getQuote() {
+
+        var quotes = [
+          "I would rather have an Agave bottle in front of me than a frontal lobatomy.",
+          "Better to remain silent and be thought a fool than to speak and erase all doubt.",
+          "A two-year-old is kind of like having a blender, but you don't have a top for it.",
+          "Between two evils, I always pick the one I never tried before."
+        ];
+
+        var index = Math.floor(Math.random() * quotes.length);
+        return quotes[index];
+    }
+
+    function cmdGetContent() {
+        // display quote inside Agave
+        $("#contentArea").html(getQuote());
+    }
+
+    function cmdInsertContent() {
+        // insert current quote in Active Word document
+        var quote = $("#contentArea").html();
+        Office.context.document.setSelectedDataAsync(quote, {}, function () { });
+    }
+
+})();
