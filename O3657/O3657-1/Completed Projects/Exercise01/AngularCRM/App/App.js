6bc2bc32f889970280235e0da1fdb063bcb83b44
Todd Baginski <topshelftbag@hotmail.com>
2016-01-04 15:34:10 -0500

    O3657-1: Updated and enhanced slide deck and upgraded projects and documentation to VS 2015.  Converted the code to use the Office UI Fabric. This commit addresses issues #160 and #117.

---
+++ O3657/O3657-1/Completed Projects/Exercise01/AngularCRM/App/App.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+var crmApp = angular.module("AngularCRM", []);
+
+crmApp.config(function () {
+    var hostWeb = $.getQueryStringValue("SPHostUrl");
+    $("#lnkHostWeb").attr("href", hostWeb);
+});
