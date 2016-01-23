76c0ffdb4cc7d478751ec5009e05d0385a16744c
daniel laskewitz <daniel.laskewitz@sogeti.nl>
2016-01-23 16:16:24 +0100

    Added addincompose and addincompose to the solution, because it's referenced in the readme.

---
+++ O3652/O3652-1 Deep Dive in Office Outlook Add-ins/Completed Exercises/Exercise01/FirstMailAddinWeb/AddInCompose/Home/Home.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    'use strict';
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#set-subject').click(setSubject);
+            $('#get-subject').click(getSubject);
+            $('#add-to-recipients').click(addToRecipients);
+        });
+    };
+
+    function setSubject() {
+        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.setAsync("Hello world!");
+    }
+
+    function getSubject() {
+        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.getAsync(function (result) {
+            app.showNotification('The current subject is', result.value)
+        });
+    }
+
+    function addToRecipients() {
+        var item = Office.context.mailbox.item;
+        var addressToAdd = {
+            displayName: Office.context.mailbox.userProfile.displayName,
+            emailAddress: Office.context.mailbox.userProfile.emailAddress
+        };
+ 
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            Office.cast.item.toMessageCompose(item).to.addAsync([addressToAdd]);
+        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
+            Office.cast.item.toAppointmentCompose(item).requiredAttendees.addAsync([addressToAdd]);
+        }
+    }
+
+})();
---
+++ O3652/O3652-1 Deep Dive in Office Outlook Add-ins/Completed Exercises/Exercise01/FirstMailAddinWeb/AddInRead/Home/Home.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The Office initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            displayItemDetails();
+        });
+    };
+
+    // Displays the "Subject" and "From" fields, based on the current mail item
+    function displayItemDetails() {
+        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
+        $('#subject').text(item.subject);
+
+        var from;
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            from = Office.cast.item.toMessageRead(item).from;
+        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
+            from = Office.cast.item.toAppointmentRead(item).organizer;
+        }
+
+        if (from) {
+            $('#from').text(from.displayName);
+            $('#from').click(function () {
+                app.showNotification(from.displayName, from.emailAddress);
+            });
+        }
+    }
+})();
