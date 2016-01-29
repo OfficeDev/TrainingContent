5c0d076f5560c937d0d68484f745efacd747500f
Todd Baginski <topshelftbag@hotmail.com>
2016-01-29 12:25:21 -0500

    O3653-3: Converted to Visual Studio 2015 projects and Graph REST API. Created separate completed project for each lab exercise (where applicable). Updated app -> Add-in text in slide deck. Adjusted URLs in slide deck for Graph API. Added slide about direct endpoints. Updated demo readme. Updated all images.  Removed obsolete images. Updated lab.md file screenshots and verbiage. Addresses issues #248, #247, #246, #245, #228, #184, #183, #156, #141 and #117.

---
+++ O3653/O3653-3 Deep Dive into Office 365 APIs for OneDrive for Business/Completed Projects/Exercise02/OneDriveWeb/Controllers/FilesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using OneDriveWeb.Models;
+using System.Threading.Tasks;
+using OneDriveWeb.Models.JsonHelpers;
+
+namespace OneDriveWeb.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        [Authorize]
+        public async Task<ActionResult> Index(int? pageIndex, int? pageSize)
+        {
+
+            FileRepository repository = new FileRepository();
+
+            // setup paging defaults if not provided
+            pageIndex = pageIndex ?? 0;
+            pageSize = pageSize ?? 10;
+
+            // setup paging for the IU
+            ViewBag.PageIndex = (int)pageIndex;
+            ViewBag.PageSize = (int)pageSize;
+
+            var results = await repository.GetMyFiles((int)pageIndex, (int)pageSize);
+
+            return View(results);
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Upload()
+        {
+
+            FileRepository repository = new FileRepository();
+
+            foreach (string key in Request.Files)
+            {
+                if (Request.Files[key] != null && Request.Files[key].ContentLength > 0)
+                {
+                    var file = await repository.UploadFile(
+                        Request.Files[key].InputStream,
+                        Request.Files[key].FileName.Split('\\')[Request.Files[key].FileName.Split('\\').Length - 1]);
+                }
+            }
+
+            return Redirect("/Files");
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Delete(string name, string etag)
+        {
+            FileRepository repository = new FileRepository();
+
+            if (name != null)
+            {
+                await repository.DeleteFile(name, etag);
+            }
+
+            return Redirect("/Files");
+
+        }
+    }
+}
---
+++ O3653/O3653-3 Deep Dive into Office 365 APIs for OneDrive for Business/Completed Projects/Exercise02/OneDriveWeb/Models/FileRepository.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Diagnostics;
+using OneDriveWeb.Models.JsonHelpers;
+
+namespace OneDriveWeb.Models
+{
+    public class FileRepository
+    {
+        private string GraphResourceUrl = "https://graph.microsoft.com/V1.0/";
+
+        public static async Task<string> GetGraphAccessTokenAsync()
+        {
+            var AzureAdGraphResourceURL = "https://graph.microsoft.com/";
+            var Authority = ConfigurationManager.AppSettings["ida:AADInstance"] + ConfigurationManager.AppSettings["ida:TenantId"];
+
+            var signInUserId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var userObjectId = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+            var clientCredential = new ClientCredential(ConfigurationManager.AppSettings["ida:ClientId"], ConfigurationManager.AppSettings["ida:ClientSecret"]);
+            var userIdentifier = new UserIdentifier(userObjectId, UserIdentifierType.UniqueId);
+
+            // create auth context
+            AuthenticationContext authContext = new AuthenticationContext(Authority, new ADALTokenCache(signInUserId));
+            var result = await authContext.AcquireTokenSilentAsync(AzureAdGraphResourceURL, clientCredential, userIdentifier);
+
+            return result.AccessToken;
+        }
+
+        public async Task<IEnumerable<FolderItem>> GetMyFiles(int pageIndex, int pageSize)
+        {
+            // create the query for all file at the root
+            var query = GraphResourceUrl + "me/drive/root/children";
+            // issue request & get response
+            string responseString = await GetJsonAsync(query);
+            // convert them to JSON
+            var folderContents = JsonConvert.DeserializeObject<FolderContents>(responseString);
+
+            return folderContents.FolderItems.OrderBy(item => item.Name).Skip(pageIndex * pageSize).Take(pageSize);
+        }
+
+        public static async Task<string> GetJsonAsync(string url)
+        {
+            string accessToken = await GetGraphAccessTokenAsync();
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                using (var response = await client.GetAsync(url))
+                {
+                    if (response.IsSuccessStatusCode)
+                        return await response.Content.ReadAsStringAsync();
+                    return null;
+                }
+            }
+        }
+
+        public async Task<bool> DeleteFile(string id, string etag)
+        {
+            // create query request to delete file
+            var query = GraphResourceUrl + "/me/drive/items/" + id;
+            string accessToken = await GetGraphAccessTokenAsync();
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                client.DefaultRequestHeaders.IfMatch.Add(new EntityTagHeaderValue(etag));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                using (var response = await client.DeleteAsync(query))
+                {
+                    if (response.IsSuccessStatusCode)
+                        return true;
+                    else
+                        Debug.WriteLine("DeleteMessage error: " + response.StatusCode);
+                }
+            }
+
+            return false;
+        }
+
+        public async Task<FolderItem> UploadFile(System.IO.Stream filestream, string filename)
+        {
+            // create query request to delete file
+            var query = GraphResourceUrl + "me/drive/root:/" + filename + ":/content";
+            string accessToken = await GetGraphAccessTokenAsync();
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                using (var content = new StreamContent(filestream))
+                {
+                    content.Headers.Add("Content-Type", "text/plain");
+                    using (var response = await client.PutAsync(query, content))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            return JsonConvert.DeserializeObject<FolderItem>(await response.Content.ReadAsStringAsync());
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
