665d501d0b2339f68fa14bccafde2a5e81091f7b
Todd Baginski <topshelftbag@hotmail.com>
2016-03-02 17:39:32 -0500

    O3657-2: Updated lab.md.  Added image.  Updated .ts and .html files.  This fixes issues #281, #282, #290.

--- O3657/O3657-2 Creating Client Side Only Apps with Angular, ADAL and Office 365 APIs/Completed Project/Exercise05/src/app/expenseApp/app.routes.ts
+++ O3657/O3657-2 Creating Client Side Only Apps with Angular, ADAL and Office 365 APIs/Completed Project/Exercise05/src/app/expenseApp/app.routes.ts
@@ -19,31 +19,31 @@ module expenseApp {
           templateUrl:    viewBase + 'employees/employees.html',
           controllerAs:   'vm',
           requireADLogin: true
-        })
+        } as ng.route.IRoute)
         .when('/employeeExpenses/:employeeId', {
           controller:     'expenseApp.employees.EmployeeExpensesController',
           templateUrl:    viewBase + 'employees/employeeExpenses.html',
           controllerAs:   'vm',
           requireADLogin: true
-        })
+        } as ng.route.IRoute)
         .when('/employeeEdit/:employeeId', {
           controller:     'expenseApp.employees.EmployeeEditController',
           templateUrl:    viewBase + 'employees/employeeEdit.html',
           controllerAs:   'vm',
           requireADLogin: true
-        })
+        } as ng.route.IRoute)
         .when('/expenses', {
           controller:     'expenseApp.expenses.ExpensesController',
           templateUrl:    viewBase + 'expenses/expenses.html',
           controllerAs:   'vm',
           requireADLogin: true
-        })
+        } as ng.route.IRoute)
         .when('/expensesAttachReceipt/:employeeId/:expenseId', {
           templateUrl:    viewBase + 'expenses/expensesAttachReceipt.html',
           controller:     'expenseApp.expenses.ExpenseReceiptController',
           controllerAs:   'vm',
           requireADLogin: true
-        })
+        } as ng.route.IRoute)
         .when('/about', {
           controller:  'expenseApp.AboutController',
           templateUrl: viewBase + 'about.html'
