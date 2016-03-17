61d40dfb46f9d08282c07fe5073ecc2d8ac3f4d1
Todd Baginski <topshelftbag@hotmail.com>
2016-03-17 12:55:25 -0400

    O3653-16 Adding new module: Excel - Microsoft Graph.  This addresses issue #256.

---
+++ O3653/O3653-16 Excel - Microsoft Graph/Completed Solutions/Exercise 4/GraphExcel - Copy/GraphExcel/Controllers/ToDoListController.cs
@@ -0,0 +1,64 @@
+﻿using System.Web.Mvc;
+using System.Threading.Tasks;
+using GraphExcel.Helpers;
+
+namespace GraphExcel.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        ExcelAPIHelper _restAPIHelper = new ExcelAPIHelper();
+
+        [Authorize]
+        public async Task<ActionResult> Index()
+        {
+            return View(await _restAPIHelper.GetToDoItems());
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var priorityList = new SelectList(new[]
+                                          {
+                                          new {ID="1",Name="High"},
+                                          new{ID="2",Name="Normal"},
+                                          new{ID="3",Name="Low"},
+                                      },
+                            "ID", "Name", 1);
+            ViewData["priorityList"] = priorityList;
+
+            var statusList = new SelectList(new[]
+                              {
+                                          new {ID="1",Name="Not started"},
+                                          new{ID="2",Name="In-progress"},
+                                          new{ID="3",Name="Completed"},
+                                      },
+                "ID", "Name", 1);
+            ViewData["statusList"] = statusList;
+
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult> Create(FormCollection collection)
+        {
+            try
+            {
+
+                await _restAPIHelper.CreateToDoItem(
+                    collection["Title"],
+                    collection["PriorityDD"],
+                    collection["StatusDD"],
+                    collection["PercentComplete"],
+                    collection["StartDate"],
+                    collection["EndDate"],
+                    collection["Notes"]);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
