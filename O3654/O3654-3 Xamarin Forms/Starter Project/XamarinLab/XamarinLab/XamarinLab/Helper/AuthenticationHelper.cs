a16801e0b2f9484bb2a7c3dfd201b7a8261ac9dc
Todd Baginski <todd@toddbaginski.com>
2017-03-08 12:49:13 -0500

    Renamed Starer Project folder to Starter Project, updated lab.md to match renamed folder, renamed Cantacts to Contacts in ContactsPage.xaml.cs.

---
+++ O3654/O3654-3 Xamarin Forms/Starter Project/XamarinLab/XamarinLab/XamarinLab/Helper/AuthenticationHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace XamarinLab.Helper
+{
+    class AuthenticationHelper
+    {
+        public static string userToken = null;
+        public static DateTimeOffset expiration;
+    }
+}
