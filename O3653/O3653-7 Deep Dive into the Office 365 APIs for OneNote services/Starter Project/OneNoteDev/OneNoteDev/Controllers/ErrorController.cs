b0f808b1d62d64c80ea9a5e9ffadd305e3bc28e3
Todd Baginski <todd@toddbaginski.com>
2017-05-31 10:17:29 -0400

    Addresses issue #426

---
+++ O3653/O3653-7 Deep Dive into the Office 365 APIs for OneNote services/Starter Project/OneNoteDev/OneNoteDev/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+
+namespace OneNoteDev.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            if (Request["message"] != null)
+            {
+                TempData["message"] = Request["message"];
+            }
+            return View("Error");
+        }
+       
+    }
+}
