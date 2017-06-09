4c660e182374cce6efb77b5b0438525c3b791d36
Todd Baginski <todd@toddbaginski.com>
2017-06-09 11:09:47 -0400

    Addresses issue #421

---
+++ O3653/O3653-19 WebHooks/Completed Projects/Exercise 2/GraphWebhooks/GraphWebhooks/SignalR/NotificationHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace GraphWebhooks.SignalR
+{
+    public class NotificationHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
