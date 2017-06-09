037d0678999f1621929f077d6cdb14507e548b44
Todd Baginski <todd@toddbaginski.com>
2017-06-09 11:09:47 -0400

    Addresses issue #421

---
+++ O3653/O3653-19 WebHooks/Completed Projects/Exercise 4/GraphWebhooks/GraphWebhooks/Startup.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.IdentityModel.Tokens;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols;
+using System.Security.Claims;
+using GraphWebhooks.TokenStorage;
+using GraphWebhooks.Auth;
+
+[assembly: OwinStartup(typeof(GraphWebhooks.Startup))]
+
+namespace GraphWebhooks
+{
+    public class Startup
+    {
+        public static string appId = ConfigurationManager.AppSettings["ida:AppId"];
+        public static string appSecret = ConfigurationManager.AppSettings["ida:AppSecret"];
+        public static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+
+        public static string[] scopes = ConfigurationManager.AppSettings["ida:AppScopes"]
+          .Replace(' ', ',').Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+              new OpenIdConnectAuthenticationOptions
+              {
+                  // The `Authority` represents the v2.0 endpoint - https://login.microsoftonline.com/common/v2.0
+                  // The `Scope` describes the permissions that your app will need.  
+                  // See https://azure.microsoft.com/documentation/articles/active-directory-v2-scopes/
+                  // The 'ResponseType' indicates that we want an authorization code and an ID token 
+                  // In a real application you could use issuer validation for additional checks, like making 
+                  // sure the user's organization has signed up for your app, for instance.
+
+                  ClientId = appId,
+                  Authority = string.Format(CultureInfo.InvariantCulture, aadInstance, "common", "/v2.0"),
+                  Scope = "openid offline_access profile " + string.Join(" ", scopes),
+                  ResponseType = "code id_token",
+                  RedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"],
+                  PostLogoutRedirectUri = "/",
+                  TokenValidationParameters = new TokenValidationParameters
+                  {
+                      // For demo purposes only, see below
+                      ValidateIssuer = false
+
+                      // In a real multitenant app, you would add logic to determine whether the
+                      // issuer was from an authorized tenant
+                      //ValidateIssuer = true,
+                      //IssuerValidator = (issuer, token, tvp) =>
+                      //{
+                      //  if (MyCustomTenantValidation(issuer))
+                      //  {
+                      //    return issuer;
+                      //  }
+                      //  else
+                      //  {
+                      //    throw new SecurityTokenInvalidIssuerException("Invalid issuer");
+                      //  }
+                      //}
+                  },
+                  Notifications = new OpenIdConnectAuthenticationNotifications
+                  {
+                      AuthenticationFailed = OnAuthenticationFailed,
+                      AuthorizationCodeReceived = OnAuthorizationCodeReceived
+                  }
+              }
+            );
+        }
+
+        private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage,
+          OpenIdConnectAuthenticationOptions> notification)
+        {
+            notification.HandleResponse();
+            notification.Response.Redirect("/Error?message=" + notification.Exception.Message);
+            return Task.FromResult(0);
+        }
+
+        private async Task OnAuthorizationCodeReceived(AuthorizationCodeReceivedNotification notification)
+        {
+
+            // Get the user's object id (used to name the token cache)
+            ClaimsPrincipal principal = new ClaimsPrincipal(notification.AuthenticationTicket.Identity);
+            string userObjId = AuthHelper.GetUserId(principal);
+
+            // Create a token cache
+            RuntimeTokenCache tokenCache = new RuntimeTokenCache(userObjId);
+
+            // Exchange the auth code for a token
+            AuthHelper authHelper = new AuthHelper(tokenCache);
+
+            var response = await authHelper.GetTokensFromAuthority("authorization_code", notification.Code,
+                notification.Request.Uri.ToString());
+        }
+    }
+}
---
+++ O3653/O3653-19 WebHooks/Starter Project/GraphWebhooks/GraphWebhooks/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using GraphWebhooks.Auth;
+using GraphWebhooks.TokenStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraphWebhooks.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public async Task<ActionResult> Graph()
+        {
+           
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendGraphRequest(string accessToken, string requestUrl)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Set up the HTTP GET request
+                HttpRequestMessage apiRequest = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+                apiRequest.Headers.UserAgent.Add(new ProductInfoHeaderValue("OAuthStarter", "1.0"));
+                apiRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                apiRequest.Headers.Add("client-request-id", Guid.NewGuid().ToString());
+                apiRequest.Headers.Add("return-client-request-id", "true");
+
+                // Send the request and return the JSON body of the response
+                HttpResponseMessage response = await httpClient.SendAsync(apiRequest);
+                return Json(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
