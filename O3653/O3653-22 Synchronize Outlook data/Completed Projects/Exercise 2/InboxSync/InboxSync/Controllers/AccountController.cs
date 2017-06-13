31caa285d6f6b6e958a57e982777c2beba87af42
Todd Baginski <todd@toddbaginski.com>
2017-06-13 10:23:58 -0400

    Updates to address issue #424

---
+++ O3653/O3653-22 Synchronize Outlook data/Completed Projects/Exercise 2/InboxSync/InboxSync/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using InboxSync.TokenStorage;
+using InboxSync.Helpers;
+using System.Security.Claims;
+
+namespace InboxSync.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // Signal OWIN to send an authorization request to Azure.
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                  new AuthenticationProperties { RedirectUri = "/" },
+                  OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        // Here we just clear the token cache, sign out the GraphServiceClient, and end the session with the web app.  
+        public void SignOut()
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Get the user's token cache and clear it.
+                string userObjectId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                SessionTokenCache tokenCache = new SessionTokenCache(userObjectId, HttpContext);
+                tokenCache.Clear(userObjectId);
+            }
+
+            SDKHelper.SignOutClient();
+
+            // Send an OpenID Connect sign-out request. 
+            HttpContext.GetOwinContext().Authentication.SignOut(
+              CookieAuthenticationDefaults.AuthenticationType);
+            Response.Redirect("/");
+        }
+    }
+}
