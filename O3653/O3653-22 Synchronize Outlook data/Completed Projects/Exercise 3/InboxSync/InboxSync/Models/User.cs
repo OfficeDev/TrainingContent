3747a0e11d306e7e568a5d71ab32a8a03c6e558a
Todd Baginski <todd@toddbaginski.com>
2017-06-13 10:23:58 -0400

    Updates to address issue #424

---
+++ O3653/O3653-22 Synchronize Outlook data/Completed Projects/Exercise 3/InboxSync/InboxSync/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace InboxSync.Models
+{
+    public class User
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+    }
+}
