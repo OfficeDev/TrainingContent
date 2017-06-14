6d839ea4eca0f2e826f8362ba348ad09520a6ed9
Todd Baginski <todd@toddbaginski.com>
2017-06-14 10:57:48 -0400

    Updates to address issue #420

---
+++ O3653/O3653-18 People/StarterFiles/PeopleGraphWeb/PeopleGraphWeb/Models/People.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace PeopleGraphWeb.Models
+{
+    public class People
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("department")]
+        public string Department { get; set; }
+    }
+}
