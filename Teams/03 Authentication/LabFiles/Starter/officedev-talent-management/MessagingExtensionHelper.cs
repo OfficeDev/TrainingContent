e3153adca0f75ced44ac4c038c7ce49c1a3cb5b4
Paul Schaeflein <paul@schaeflein.net>
2018-11-14 07:54:42 -0600

    Changed starter from tabs to spaces

--- Teams/03 Authentication/LabFiles/Starter/officedev-talent-management/MessagingExtensionHelper.cs
+++ Teams/03 Authentication/LabFiles/Starter/officedev-talent-management/MessagingExtensionHelper.cs
@@ -5,7 +5,6 @@ using Newtonsoft.Json.Linq;
 using OfficeDev.Talent.Management;
 using System.Globalization;
 using System.Threading.Tasks;
-/////////
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,78 +12,78 @@ using System.Web;
 
 namespace officedev_talent_management
 {
-	public class MessagingExtensionHelper
-	{
-		public static async Task<ComposeExtensionResponse> CreateResponse(Activity activity)
-		{
-			ComposeExtensionResponse response = null;
+  public class MessagingExtensionHelper
+  {
+    public static async Task<ComposeExtensionResponse> CreateResponse(Activity activity)
+    {
+      ComposeExtensionResponse response = null;
 
-			var query = activity.GetComposeExtensionQueryData();
-			JObject data = activity.Value as JObject;
+      var query = activity.GetComposeExtensionQueryData();
+      JObject data = activity.Value as JObject;
 
-			//Check to make sure a query was actually made:
-			if (query.CommandId == null || query.Parameters == null)
-			{
-				return null;
-			}
-			else if (query.Parameters.Count > 0)
-			{
-				// query.Parameters has the parameters sent by client
-				var results = new ComposeExtensionResult()
-				{
-					AttachmentLayout = "list",
-					Type = "result",
-					Attachments = new List<ComposeExtensionAttachment>(),
-				};
+      //Check to make sure a query was actually made:
+      if (query.CommandId == null || query.Parameters == null)
+      {
+        return null;
+      }
+      else if (query.Parameters.Count > 0)
+      {
+        // query.Parameters has the parameters sent by client
+        var results = new ComposeExtensionResult()
+        {
+          AttachmentLayout = "list",
+          Type = "result",
+          Attachments = new List<ComposeExtensionAttachment>(),
+        };
 
-				if (query.CommandId == "searchPositions")
-				{
-					OpenPositionsDataController controller = new OpenPositionsDataController();
-					IEnumerable<OpenPosition> positions;
+        if (query.CommandId == "searchPositions")
+        {
+          OpenPositionsDataController controller = new OpenPositionsDataController();
+          IEnumerable<OpenPosition> positions;
 
-					if (query.Parameters[0].Name == "initialRun")
-					{
-						// Default query => list all
-						positions = controller.ListOpenPositions(10);
-					}
-					else
-					{
-						// Basic search.
-						string title = query.Parameters[0].Value.ToString().ToLower();
-						positions = controller.ListOpenPositions(10).Where(x => x.Title.ToLower().Contains(title));
-					}
+          if (query.Parameters[0].Name == "initialRun")
+          {
+            // Default query => list all
+            positions = controller.ListOpenPositions(10);
+          }
+          else
+          {
+            // Basic search.
+            string title = query.Parameters[0].Value.ToString().ToLower();
+            positions = controller.ListOpenPositions(10).Where(x => x.Title.ToLower().Contains(title));
+          }
 
-					// Generate cards for the response.
-					foreach (OpenPosition pos in positions)
-					{
-						var card = CardHelper.CreateCardForPosition(pos, true);
+          // Generate cards for the response.
+          foreach (OpenPosition pos in positions)
+          {
+            var card = CardHelper.CreateCardForPosition(pos, true);
 
-						var composeExtensionAttachment = card.ToAttachment().ToComposeExtensionAttachment();
-						results.Attachments.Add(composeExtensionAttachment);
-					}
-				}
-				else if (query.CommandId == "searchCandidates")
-				{
-					string name = query.Parameters[0].Value.ToString();
-					CandidatesDataController controller = new CandidatesDataController();
+            var composeExtensionAttachment = card.ToAttachment().ToComposeExtensionAttachment();
+            results.Attachments.Add(composeExtensionAttachment);
+          }
+        }
+        else if (query.CommandId == "searchCandidates")
+        {
+          string name = query.Parameters[0].Value.ToString();
+          CandidatesDataController controller = new CandidatesDataController();
 
-					foreach (Candidate c in controller.GetTopCandidates("ABCD1234"))
-					{
-						c.Name = c.Name.Split(' ')[0] + " " + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
-						var card = CardHelper.CreateSummaryCardForCandidate(c);
+          foreach (Candidate c in controller.GetTopCandidates("ABCD1234"))
+          {
+            c.Name = c.Name.Split(' ')[0] + " " + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+            var card = CardHelper.CreateSummaryCardForCandidate(c);
 
-						var composeExtensionAttachment = card.ToAttachment().ToComposeExtensionAttachment(CardHelper.CreatePreviewCardForCandidate(c).ToAttachment());
-						results.Attachments.Add(composeExtensionAttachment);
-					}
-				}
+            var composeExtensionAttachment = card.ToAttachment().ToComposeExtensionAttachment(CardHelper.CreatePreviewCardForCandidate(c).ToAttachment());
+            results.Attachments.Add(composeExtensionAttachment);
+          }
+        }
 
-				response = new ComposeExtensionResponse()
-				{
-					ComposeExtension = results
-				};
-			}
+        response = new ComposeExtensionResponse()
+        {
+          ComposeExtension = results
+        };
+      }
 
-			return response;
-		}
-	}
+      return response;
+    }
+  }
 }
