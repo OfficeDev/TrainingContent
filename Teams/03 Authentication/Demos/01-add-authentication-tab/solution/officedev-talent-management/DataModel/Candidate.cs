796c253cabd37b587b56e762d5053de6376d24a7
Paul Schaeflein <paul@schaeflein.net>
2018-11-13 20:57:28 -0600

    update demo1

---
+++ Teams/03 Authentication/Demos/01-add-authentication-tab/solution/officedev-talent-management/DataModel/Candidate.cs
@@ -0,0 +1,13 @@
+﻿namespace OfficeDev.Talent.Management
+{
+	public class Candidate
+    {
+        public string Name { get; set; }
+        public int Hires { get; set; }
+        public int NoHires { get; set; }
+        public string Stage { get; set; }
+        public string ReqId { get; set; }
+        public string CurrentRole { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
