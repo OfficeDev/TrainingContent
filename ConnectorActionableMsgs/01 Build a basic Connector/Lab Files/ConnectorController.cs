09e958afe1147a6ca0b8da4b60662eb511ce508e
Paul Schaeflein <paul@schaeflein.net>
2018-12-12 09:10:47 -0600

    Add lab files

---
+++ ConnectorActionableMsgs/01 Build a basic Connector/Lab Files/ConnectorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using ToDoConnector.Models;
+
+namespace ToDoConnector.Controllers
+{
+	public class ConnectorController : ApiController
+	{
+		[HttpPost]
+		public async Task<IHttpActionResult> Register(Subscription subscription)
+		{
+			await TaskHelper.PostWelcomeMessage(subscription.WebHookUri, subscription.GroupName);
+			return Ok();
+		}
+	}
+}
