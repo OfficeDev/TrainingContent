2bef76ee157cbe2799ef2e69b71fa7e918bded90
Paul Schaeflein <paul@schaeflein.net>
2018-12-12 09:28:20 -0600

    Tabs to spaces...

---
+++ ConnectorActionableMsgs/01 Build a basic Connector/Demos/02-webconnector/ToDoConnector/Models/ConnectorSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoConnector.Models
+{
+  public class ConnectorSettings
+  {
+    public string AppType { get; set; }
+    public string ConfigName { get; set; }
+    public string ContentUrl { get; set; }
+    public string EntityId { get; set; }
+    public string UserObjectId { get; set; }
+    public string WebHookUrl { get; set; }
+  }
+}
