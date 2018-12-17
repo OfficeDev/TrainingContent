8a848839a992c715881037b3c2248bc6faf36aae
Paul Schaeflein <paul@schaeflein.net>
2018-12-17 11:16:54 -0600

    Updated completed app in LabFiles

---
+++ ConnectorActionableMsgs/03 Adaptive Cards/LabFiles/RefreshAdaptiveCard/Completed/CardRefresh/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CardRefresh.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ConnectorActionableMsgs/03 Adaptive Cards/LabFiles/RefreshAdaptiveCard/Completed/CardRefresh/Models/CardResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardRefresh.Models
+{
+	public class CardResponse
+	{
+		public string Comment { get; set; }
+		public string CachedComments { get; set; }
+	}
+}
