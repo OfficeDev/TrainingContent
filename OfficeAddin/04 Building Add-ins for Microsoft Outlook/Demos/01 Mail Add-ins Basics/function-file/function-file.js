3cdb4251a01a68fce96390b21976f9fe92b243c2
David Chesnut <davech@microsoft.com>
2019-02-04 10:19:12 -0800

    Added copyright comments

--- OfficeAddin/04 Building Add-ins for Microsoft Outlook/Demos/01 Mail Add-ins Basics/function-file/function-file.js
+++ OfficeAddin/04 Building Add-ins for Microsoft Outlook/Demos/01 Mail Add-ins Basics/function-file/function-file.js
@@ -1,7 +1,5 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 // The initialize function must be run each time a new page is loaded
 Office.initialize = reason => {
