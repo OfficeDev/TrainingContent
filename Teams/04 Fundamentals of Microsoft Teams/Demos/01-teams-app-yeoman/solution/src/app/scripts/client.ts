8e7d315c42226a2798fd1e58eb3a90469edbaa3d
Andrew Connell <me@andrewconnell.com>
2020-02-20 15:03:31 -0500

    Archive outdated MSTeams modules (#672)
    - in FY20, new modules were created with the intention to replace the existing outdated modules
    - the outdated modules were archived off to the OfficeDev/TrainingContent-Archive repo

--- Teams/04 Fundamentals of Microsoft Teams/Demos/01-teams-app-yeoman/solution/src/app/scripts/client.ts
+++
@@ -1,4 +0,0 @@
-// Automatically added for the teamsApp1Tab tab
-export * from "./teamsApp1Tab/TeamsApp1Tab";
-export * from "./teamsApp1Tab/TeamsApp1TabConfig";
-export * from "./teamsApp1Tab/TeamsApp1TabRemove";
--- Teams/04 Fundamentals of Microsoft Teams/Demos/02-teams-bot-visual-studio/solution/teams-bot1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="teams_bot1.WebApiApplication" Language="C#" %>
