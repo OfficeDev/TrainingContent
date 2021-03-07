482ea365db58f8a6df268c2177ea5a2709f8be39
Paul Schaeflein <paul@schaeflein.net>
2021-03-07 11:17:15 -0600

    MSTeams - ConversationBots - FY21Q3 refresh
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - updated slides where necessary
    - update code samples to reflect MSLearn HOL exercise refresh & updates
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft

---
+++ Teams/40 Conversational Bots/Demos/01-learn-msteams-bots/src/server/conversationalBot/dialogs/HelpDialog.ts
@@ -0,0 +1,8 @@
+import { Dialog, DialogContext, DialogTurnResult } from "botbuilder-dialogs";
+
+export default class HelpDialog extends Dialog {
+  public async beginDialog(context: DialogContext, options?: any): Promise<DialogTurnResult> {
+    context.context.sendActivity("I'm just a friendly but rather stupid bot, and right now I don't have any valuable help for you!");
+    return await context.endDialog();
+  }
+}
