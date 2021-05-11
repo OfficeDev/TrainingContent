d77ae7e52cc30f85185181ae8e131733a6d3fb8a
Paul Schaeflein <paul@schaeflein.net>
2021-05-11 08:06:38 -0500

    FY21Q4 refresh - msteams-task-modules
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - updated slides where necessary
    - update code samples to reflect MSLearn HOL exercise refresh & updates
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft

--- Teams/50 Task Modules/Demos/02-adaptivecard-taskmodule/src/client/youTubePlayer1Tab/YouTubePlayer1Tab.tsx
+++ Teams/50 Task Modules/Demos/02-adaptivecard-taskmodule/src/client/youTubePlayer1Tab/YouTubePlayer1Tab.tsx
@@ -54,6 +54,7 @@ export const YouTubePlayer1Tab = () => {
     };
 
     const submitHandler = (err: string, result: string): void => {
+      console.log(`Submit handler - err: ${err}`);
       setYouTubeVideoId(result);
     };
 
@@ -82,6 +83,7 @@ export const YouTubePlayer1Tab = () => {
     };
 
     const submitHandler = (err: string, result: any): void => {
+      console.log(`Submit handler - err: ${err}`);
       setYouTubeVideoId(result.youTubeVideoId);
     };
 
