9b7970cf6cec6442518712d7f2b4513a223e0b05
Paul Schaeflein <paul.schaeflein@addin365.com>
2022-09-12 07:26:26 -0500

    FY23Q1 refresh - msteams-task-modules
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - updated slides where necessary
    - update code samples to reflect MSLearn HOL exercise refresh & updates
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft

--- Teams/50 Task Modules/Demos/03-bot-taskmodule/src/server/TeamsAppsComponents.ts
+++ Teams/50 Task Modules/Demos/03-bot-taskmodule/src/server/TeamsAppsComponents.ts
@@ -2,4 +2,5 @@
 export const nonce = {}; // Do not remove!
 // Automatically added for the youTubePlayer1Tab tab
 export * from "./youTubePlayer1Tab/YouTubePlayer1Tab";
+export * from "./youTubePlayer1Tab/VideoSelectorTaskModule";
 export * from "./learningTeamsBot/learningTeamsBot";
