ae76e479b519cb8af409e7c70767a1450f217d20
Andrew Connell <me@andrewconnell.com>
2022-11-30 19:21:01 -0500

    FY23Q2 refresh - embedded-web-experiences
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - updated slides where necessary
    - update code samples to reflect MSLearn HOL exercise refresh & updates
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft

--- Teams/30 Create Embedded Web Experiences with Tabs for Microsoft Teams/Demos/02-learn-msteams-tabs/src/client/configMathTab/ConfigMathTab.tsx
+++ Teams/30 Create Embedded Web Experiences with Tabs for Microsoft Teams/Demos/02-learn-msteams-tabs/src/client/configMathTab/ConfigMathTab.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { Provider, Flex, Text, Button, Header, Input, InputProps } from "@fluentui/react-northstar"; 
+import { Provider, Flex, Text, Button, Header, Input, InputProps } from "@fluentui/react-northstar";
 import { useState, useEffect } from "react";
 import { useTeams } from "msteams-react-base-component";
 import { app } from "@microsoft/teams-js";
 
 export interface IConfigMathTabState {
-  mathOperator?: string;
+  mathOperator ?: string;
   operandA: number;
   operandB: number;
   result: string;
@@ -16,106 +16,106 @@ export interface IConfigMathTabState {
  */
 export const ConfigMathTab = () => {
 
-  const [{ inTeams, theme, context }] = useTeams();
-  const [entityId, setEntityId] = useState<string | undefined>();
-  const [mathTabState, setMathTabState] = useState<IConfigMathTabState>({ mathOperator: "add" } as IConfigMathTabState);
+    const [{ inTeams, theme, context }] = useTeams();
+    const [entityId, setEntityId] = useState<string | undefined>();
+    const [mathTabState, setMathTabState] = useState<IConfigMathTabState>({ mathOperator: "add" } as IConfigMathTabState);
 
-  useEffect(() => {
-    if (inTeams === true) {
-      app.notifySuccess();
-    } else {
-      setEntityId("Not in Microsoft Teams");
-    }
-  }, [inTeams]);
+    useEffect(() => {
+        if (inTeams === true) {
+            app.notifySuccess();
+        } else {
+            setEntityId("Not in Microsoft Teams");
+        }
+    }, [inTeams]);
 
-  useEffect(() => {
-    if (context) {
-      setEntityId(context.page.id);
-      setMathTabState(state => ({
-        ...state,
-        mathOperator: context.page.id?.replace("MathPage", "") ?? ""
-      } as IConfigMathTabState));
-    }
-  }, [context]);
+    useEffect(() => {
+      if (context) {
+        setEntityId(context.page.id);
+        setMathTabState(state => ({
+          ...state,
+          mathOperator: context.page.id?.replace("MathPage", "") ?? ""
+        } as IConfigMathTabState));
+      }
+    }, [context]);
 
-  const handleOnChangedOperandA = (data?: InputProps): void => {
-    if (data && !isNaN(Number(data.value))) {
-      setMathTabState(state => ({
-        ...state,
-        operandA: data.value
-      } as IConfigMathTabState));
-    }
-  };
+    const handleOnChangedOperandA = (data?: InputProps): void => {
+      if (data && !isNaN(Number(data.value))) {
+        setMathTabState(state => ({
+          ...state,
+          operandA: data.value
+        } as IConfigMathTabState));
+      }
+    };
 
-  const handleOnChangedOperandB = (data?: InputProps): void => {
-    if (data && !isNaN(Number(data.value))) {
-      setMathTabState(state => ({
-        ...state,
-        operandB: data.value
-      } as IConfigMathTabState));
-    }
-  };
+    const handleOnChangedOperandB = (data?: InputProps): void => {
+      if (data && !isNaN(Number(data.value))) {
+        setMathTabState(state => ({
+          ...state,
+          operandB: data.value
+        } as IConfigMathTabState));
+      }
+    };
 
-  const handleOperandChange = (): void => {
-    let stringResult: string = "n/a";
+    const handleOperandChange = (): void => {
+      let stringResult: string = "n/a";
 
-    if (mathTabState) {
-      if (!isNaN(Number(mathTabState.operandA)) && !isNaN(Number(mathTabState.operandB))) {
-        switch (mathTabState.mathOperator) {
-          case "add":
-            stringResult = (Number(mathTabState.operandA) + Number(mathTabState.operandB)).toString();
-            break;
-          case "subtract":
-            stringResult = (Number(mathTabState.operandA) - Number(mathTabState.operandB)).toString();
-            break;
-          case "multiply":
-            stringResult = (Number(mathTabState.operandA) * Number(mathTabState.operandB)).toString();
-            break;
-          case "divide":
-            stringResult = (Number(mathTabState.operandA) / Number(mathTabState.operandB)).toString();
-            break;
-          default:
-            stringResult = "n/a";
-            break;
+      if (mathTabState) {
+        if (!isNaN(Number(mathTabState.operandA)) && !isNaN(Number(mathTabState.operandB))) {
+          switch (mathTabState.mathOperator) {
+            case "add":
+              stringResult = (Number(mathTabState.operandA) + Number(mathTabState.operandB)).toString();
+              break;
+            case "subtract":
+              stringResult = (Number(mathTabState.operandA) - Number(mathTabState.operandB)).toString();
+              break;
+            case "multiply":
+              stringResult = (Number(mathTabState.operandA) * Number(mathTabState.operandB)).toString();
+              break;
+            case "divide":
+              stringResult = (Number(mathTabState.operandA) / Number(mathTabState.operandB)).toString();
+              break;
+            default:
+              stringResult = "n/a";
+              break;
+          }
         }
       }
-    }
-    setMathTabState(state => ({
-      ...state,
-      result: stringResult
-    } as IConfigMathTabState));
-  };
+      setMathTabState(state => ({
+        ...state,
+        result: stringResult
+      } as IConfigMathTabState));
+    };
 
-  /**
-   * The render() method to create the UI of the tab
-   */
-  return (
-    <Provider theme={theme}>
-      <Flex column gap="gap.smaller">
-        <Header>This is your tab</Header>
-        <Text content="Enter the values to calculate" size="medium"></Text>
+    /**
+     * The render() method to create the UI of the tab
+     */
+    return (
+      <Provider theme={theme}>
+        <Flex column gap="gap.smaller">
+          <Header>This is your tab</Header>
+          <Text content="Enter the values to calculate" size="medium"></Text>
 
-        <Flex gap="gap.smaller">
-          <Flex.Item>
-            <Flex gap="gap.smaller">
-              <Flex.Item>
-                <Input autoFocus
-                  value={mathTabState.operandA}
-                  onChange={(e, data) => handleOnChangedOperandA(data)}></Input>
-              </Flex.Item>
-              <Text content={mathTabState.mathOperator}></Text>
-              <Flex.Item>
-                <Input value={mathTabState.operandB}
-                  onChange={(e, data) => handleOnChangedOperandB(data)}></Input>
-              </Flex.Item>
-            </Flex>
-          </Flex.Item>
-          <Button content="Calculate" primary
-            onClick={handleOperandChange}></Button>
-          <Text content={mathTabState.result}></Text>
+          <Flex gap="gap.smaller">
+            <Flex.Item>
+              <Flex gap="gap.smaller">
+                <Flex.Item>
+                  <Input autoFocus
+                    value={mathTabState.operandA}
+                    onChange={(e, data) => handleOnChangedOperandA(data)}></Input>
+                </Flex.Item>
+                <Text content={mathTabState.mathOperator}></Text>
+                <Flex.Item>
+                  <Input value={mathTabState.operandB}
+                    onChange={(e, data) => handleOnChangedOperandB(data)}></Input>
+                </Flex.Item>
+              </Flex>
+            </Flex.Item>
+            <Button content="Calculate" primary
+              onClick={handleOperandChange}></Button>
+            <Text content={mathTabState.result}></Text>
+          </Flex>
+          <Text content="(C) Copyright Contoso" size="smallest"></Text>
         </Flex>
-        <Text content="(C) Copyright Contoso" size="smallest"></Text>
-      </Flex>
-    </Provider>
-  );
+      </Provider>
+    );
 };
--- Teams/30 Create Embedded Web Experiences with Tabs for Microsoft Teams/Demos/03-auth-tab/webpack.config.js
+++ Teams/30 Create Embedded Web Experiences with Tabs for Microsoft Teams/Demos/03-auth-tab/webpack.config.js
@@ -68,7 +68,7 @@ const config = [{
         path: path.join(__dirname, "/dist/web/scripts"),
         filename: "[name].js",
         libraryTarget: "umd",
-        library: "learnMsTeamsTabs",
+        library: "learnMsTeamsAuthTabs",
         publicPath: "/scripts/"
     },
     externals: {},
